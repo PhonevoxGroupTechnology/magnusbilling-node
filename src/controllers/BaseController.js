8c0cf9503c0ed94829170f48d9ce620496015876
Adrian K. <adriankubinyete@outlook.com>
2024-12-16 09:41:30 -0300

    fix: basecontroller wrong context

--- src/controllers/BaseController.js
+++ src/controllers/BaseController.js
@@ -2,12 +2,17 @@ import Logger from '../utils/logging.js';
 
 class BaseController {
     constructor(ControllerSchema, ControllerModel) {
-        this.ControllerSchema = ControllerSchema
-        this.ControllerModel = ControllerModel;
-        this.logger = new Logger(`${ControllerModel.module}.controller`, false).useEnvConfig().create();
+        this.Schema = ControllerSchema
+        this.Model = ControllerModel;
+        this.logger = new Logger(`base.controller`, false).useEnvConfig().create();
     }
 
-    filterify(params) {
+    // functions here NEEDS to be in arrow-function form
+    // otherwise, they will lose the context of the class
+    // func = () => {}
+    // afunc = async () => {}
+
+    filterify = (params) => {
         let filter = []
         for (const [key, value] of Object.entries(params)) {
             filter.push({
@@ -20,15 +25,14 @@ class BaseController {
         return JSON.stringify(filter)
     }
 
-
     // return the zod schema of a given controller. if prioritize_api is true, prioritize the api schema over the controller schema on merge. if as_skeleton is true, return the skeleton of the schema (everything as z.any().optional()) so we can match the fields. if block_api_param has anything inside the list, block the given parameters when doing api rule parsing
-    async getSchema(options = { prioritize_api: false, merge_with: undefined, as_skeleton: false, block_api_param: [] }) {
+    getSchema = async (options = { prioritize_api: false, merge_with: undefined, as_skeleton: false, block_api_param: [] }) => {
         const { prioritize_api=false, merge_with=undefined, as_skeleton=false, block_api_param=[] } = options;
 
         let RET_SCHEMA // schema to return
 
         // get api schema
-        let API_SCHEMA = await this.ControllerModel.getRules({
+        let API_SCHEMA = await this.Model.getRules({
             as_schema: true, 
             as_skeleton: as_skeleton,
             block_param: block_api_param 
@@ -48,15 +52,15 @@ class BaseController {
         return RET_SCHEMA
     }
 
-    async getRules(req, res, next) {
+    getRules = async (req, res, next) => {
         try {
-            let rules =  await this.ControllerModel.getRules()
+            let rules =  await this.Model.getRules()
 
             if (rules) {
                 return res.status(200).json({
                     success: true,
                     message: 'Rules retrieved successfully',
-                    data: rules
+                    data: rules.response
                 })
             } else {
                 return res.status(404).json({
--- src/controllers/clients/UserController.js
+++ src/controllers/clients/UserController.js
@@ -1,154 +1,161 @@
 import UserModel from '../../models/clients/UserModel.js'
 import UserSchema from '../../schemas/clients/UserSchema.js'
+import BaseController from '../BaseController.js'
 
-const filterify = (obj) => {
-    // CHORE: this should allow you to set the comparison 
-    // and/or type of field manually.
-    let filter = []
-    for (const [key, value] of Object.entries(obj)) {
-        filter.push({
-            type: typeof(value),
-            field: key,
-            value: value,
-            comparison: 'eq'
-        })
-    }
-    return JSON.stringify(filter)
-}
+// const filterify = (obj) => {
+//     // CHORE: this should allow you to set the comparison 
+//     // and/or type of field manually.
+//     let filter = []
+//     for (const [key, value] of Object.entries(obj)) {
+//         filter.push({
+//             type: typeof(value),
+//             field: key,
+//             value: value,
+//             comparison: 'eq'
+//         })
+//     }
+//     return JSON.stringify(filter)
+// }
 
-class UserController {
-    constructor() {
-    }
+// class UserController {
+//     constructor() {
+//     }
 
-    async create(req, res, next) {
-        try {
-            let payload = req.body
+//     async create(req, res, next) {
+//         try {
+//             let payload = req.body
 
-            // Validate user data
-            let API_SCHEMA = await UserModel.getRules({ as_schema: true })
-            let SCHEMA = API_SCHEMA.merge(UserSchema.create()) // prioritize schema rules over api (email is causing trouble)
-            SCHEMA.strict().parse(payload)
+//             // Validate user data
+//             let API_SCHEMA = await UserModel.getRules({ as_schema: true })
+//             let SCHEMA = API_SCHEMA.merge(UserSchema.create()) // prioritize schema rules over api (email is causing trouble)
+//             SCHEMA.strict().parse(payload)
             
-            // Create user with validated data
-            let result = await UserModel.create(payload)
-            return res.status(result.code).json(result)
-        } catch (error) {
-            return next(error)
-        }
-    }
+//             // Create user with validated data
+//             let result = await UserModel.create(payload)
+//             return res.status(result.code).json(result)
+//         } catch (error) {
+//             return next(error)
+//         }
+//     }
 
-    async get(req, res, next) {
-        try {
-            const handlers = {
-                id: async (params) => filterify({ id: params.id }),
-                username: async (params) => filterify({ username: params.username }),
-                query: async (params) => {
-                    // validating schema structure
-                    const API_SCHEMA = await UserModel.getRules({ as_schema: true, as_skeleton: true });
-                    const SCHEMA = API_SCHEMA.merge(UserSchema.read())
-                    SCHEMA.strict().parse(params.query);
+//     async get(req, res, next) {
+//         try {
+//             const handlers = {
+//                 id: async (params) => filterify({ id: params.id }),
+//                 username: async (params) => filterify({ username: params.username }),
+//                 query: async (params) => {
+//                     // validating schema structure
+//                     const API_SCHEMA = await UserModel.getRules({ as_schema: true, as_skeleton: true });
+//                     const SCHEMA = API_SCHEMA.merge(UserSchema.read())
+//                     SCHEMA.strict().parse(params.query);
     
-                    return filterify(params.query);
-                },
-            };
+//                     return filterify(params.query);
+//                 },
+//             };
     
-            let payload;
+//             let payload;
     
-            // get the appropriate handler based on the request parameters
-            if (req.params.id) {
-                payload = await handlers.id(req.params);
-            } else if (req.params.username) {
-                payload = await handlers.username(req.params);
-            } else if (Object.keys(req.query).length > 0) {
-                payload = await handlers.query({ query: req.query });
-            } else {
-                // no payload, return everything
-                const userList = await UserModel.list();
-                return res.json(userList);
-            }
+//             // get the appropriate handler based on the request parameters
+//             if (req.params.id) {
+//                 payload = await handlers.id(req.params);
+//             } else if (req.params.username) {
+//                 payload = await handlers.username(req.params);
+//             } else if (Object.keys(req.query).length > 0) {
+//                 payload = await handlers.query({ query: req.query });
+//             } else {
+//                 // no payload, return everything
+//                 const userList = await UserModel.list();
+//                 return res.json(userList);
+//             }
     
-            // find the user
-            const filteredUserList = await UserModel.find(payload);
-            return res.json(filteredUserList);
-        } catch (error) {
-            return next(error)
-        }
-    }
+//             // find the user
+//             const filteredUserList = await UserModel.find(payload);
+//             return res.json(filteredUserList);
+//         } catch (error) {
+//             return next(error)
+//         }
+//     }
 
-    // CHORE(adrian): optimize this. i hate this implementation
-    async update(req, res, next) {
-        try {
-            const handlers = {
-                id: async (params, body) => {
-                    // validating schema structure
-                    const API_SCHEMA = await UserModel.getRules({ as_schema: true, as_skeleton: true });
-                    const SCHEMA = API_SCHEMA.merge(UserSchema.read());
-                    SCHEMA.strict().parse(body);
-                    return {id: params.id, ...body}
-                },
-                username: async (params, body) => {
-                    let userId = await UserModel.getId(filterify({ username: params.username }))
-                    return handlers.id({ id: userId }, body)
-                },
-            };
+//     // CHORE(adrian): optimize this. i hate this implementation
+//     async update(req, res, next) {
+//         try {
+//             const handlers = {
+//                 id: async (params, body) => {
+//                     // validating schema structure
+//                     const API_SCHEMA = await UserModel.getRules({ as_schema: true, as_skeleton: true });
+//                     const SCHEMA = API_SCHEMA.merge(UserSchema.read());
+//                     SCHEMA.strict().parse(body);
+//                     return {id: params.id, ...body}
+//                 },
+//                 username: async (params, body) => {
+//                     let userId = await UserModel.getId(filterify({ username: params.username }))
+//                     return handlers.id({ id: userId }, body)
+//                 },
+//             };
 
-            let payload;
-            let bodyNotEmpty = Object.keys(req.body).length > 0
+//             let payload;
+//             let bodyNotEmpty = Object.keys(req.body).length > 0
 
-            // get the appropriate handler based on the request parameters
-            if (req.params.id && bodyNotEmpty) {
-                payload = await handlers.id(req.params, req.body);
-            } else if (req.params.username && bodyNotEmpty) {
-                payload = await handlers.username(req.params, req.body);
-            } else {
-                return res.status(400).json({ error: 'Invalid request parameters' });
-            }
+//             // get the appropriate handler based on the request parameters
+//             if (req.params.id && bodyNotEmpty) {
+//                 payload = await handlers.id(req.params, req.body);
+//             } else if (req.params.username && bodyNotEmpty) {
+//                 payload = await handlers.username(req.params, req.body);
+//             } else {
+//                 return res.status(400).json({ error: 'Invalid request parameters' });
+//             }
 
-            let result = await UserModel.update(payload)
-            return res.status(result.code).json(result)
+//             let result = await UserModel.update(payload)
+//             return res.status(result.code).json(result)
 
-        } catch (error) {
-            next(error)
-        }
-    }
+//         } catch (error) {
+//             next(error)
+//         }
+//     }
 
-    async delete(req, res, next) {
-        try {
-            const handlers = {
-                id: async (params) => {
-                    return { id: params.id }
-                },
-                username: async (params) => {
-                    let userId = await UserModel.getId(filterify({ username: params.username }))
-                    return handlers.id({ id: userId })
-                },
-            };
+//     async delete(req, res, next) {
+//         try {
+//             const handlers = {
+//                 id: async (params) => {
+//                     return { id: params.id }
+//                 },
+//                 username: async (params) => {
+//                     let userId = await UserModel.getId(filterify({ username: params.username }))
+//                     return handlers.id({ id: userId })
+//                 },
+//             };
 
-            let payload;
+//             let payload;
 
-            // get the appropriate handler based on the request parameters
-            if (req.params.id) {
-                payload = await handlers.id(req.params);
-            } else if (req.params.username) {
-                payload = await handlers.username(req.params);
-            } else {
-                return res.status(400).json({ error: 'Invalid request parameters' });
-            }
+//             // get the appropriate handler based on the request parameters
+//             if (req.params.id) {
+//                 payload = await handlers.id(req.params);
+//             } else if (req.params.username) {
+//                 payload = await handlers.username(req.params);
+//             } else {
+//                 return res.status(400).json({ error: 'Invalid request parameters' });
+//             }
 
-            let ret = await UserModel.delete(payload)
-            return res.json(ret)
-        } catch (error) {
-            next(error)
-        }
-    }
+//             let ret = await UserModel.delete(payload)
+//             return res.json(ret)
+//         } catch (error) {
+//             next(error)
+//         }
+//     }
 
-    async getRules(req, res, next) {
-        let rules = await UserModel.getRules()
+//     async getRules(req, res, next) {
+//         let rules = await UserModel.getRules()
         
-        if (rules) {
-            return res.json(rules)
-        }
-        return res.status(404).json({error: 'Uh oh. No rules for this module.'})
+//         if (rules) {
+//             return res.json(rules)
+//         }
+//         return res.status(404).json({error: 'Uh oh. No rules for this module.'})
+//     }
+// }
+
+class UserController extends BaseController {
+    constructor() {
+        super(UserSchema, UserModel);
     }
 }
 
